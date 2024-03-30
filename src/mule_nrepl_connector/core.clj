6ddfa7cbcecad26c68ba695bff3b6ecbabcc233f
tmiya4ta <tmiyashita@salesforce.com>
2024-03-30 22:15:38 +0900

    Update

--- src/mule_nrepl_connector/core.clj
+++ src/mule_nrepl_connector/core.clj
@@ -14,15 +14,16 @@
   [reitit.http :as rh]
   [reitit.ring :as ring]
   [reitit.swagger-ui :as swagger-ui]
-  [org.httpkit.server :as server]
   [muuntaja.core :as m]
   [reitit.interceptor.sieppari :as sieppari]
   [reitit.http.interceptors.parameters :as params]
   [reitit.http.interceptors.muuntaja :as muuntaja]
   [taoensso.timbre :as log]
   [org.httpkit.server :as hs]
+  [org.httpkit.client :as http]
   [clojure.reflect :as cr]
   [clojure.pprint ::as pp]
+  [clojure.java.shell :refer [sh]]
   [silvur.util :refer [edn->json]])
  
  (:import [org.mule.runtime.extension.api.runtime.operation Result]
@@ -48,8 +49,11 @@
          (catch Exception e {:status 200 :body (pr-str (println e))}))))))
 
 (def route ["" {:muuntaja m/instance}
-            ["/api" {:post (gen-run-flow)}]
+            ["/api" {:post (get-run-flow)}]
             ["/ping" {:get (fn [req] {:status 200 :body "pong"})}]
+            ["/request" {:get (fn [req]
+                                {:status 200 :body (-> (dissoc req :reitit.core/match)
+                                                       (dissoc :reitit.core/router))})}]
             ["/nrepl" {:post su/nrepl-handler}]])
 
 (def app (rh/ring-handler
